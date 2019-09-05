c7695789499e5f5d4bf0d8d260c570a2ecfc75e6
wukrit <sukritwalia@gmail.com>
2019-09-05 10:05:12 -0400

    Initial commit

---
+++ db/migrate/20190905140436_create_allergies.rb
@@ -0,0 +1,10 @@
+class CreateAllergies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :allergies do |t|
+      t.integer :user_id
+      t.integer :ingredient_id
+
+      t.timestamps
+    end
+  end
+end
