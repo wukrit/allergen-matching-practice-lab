323c8007b9c179e072355409ced32699e4913ec4
wukrit <sukritwalia@gmail.com>
2019-09-05 10:54:50 -0400

    Added more seed data, established activerecord relationships

--- app/models/allergy.rb
+++ app/models/allergy.rb
@@ -1,2 +1,4 @@
 class Allergy < ApplicationRecord
+  belongs_to :user
+  belongs_to :ingredient
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,6 @@
 class Ingredient < ApplicationRecord
+  has_many :recipe_ingredients
+  has_many :allergies
+  has_many :recipes, through: :recipe_ingredients
+  has_many :users, through: :allergies
 end
